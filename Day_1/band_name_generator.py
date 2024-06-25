9474a4dfd39d57a7044ce382835e1e3e85995975
novim <novimaksim1@gmail.com>
2024-06-25 19:39:21 +0300

    update

--- Day_1/band_name_generator.py
+++ Day_1/band_name_generator.py
@@ -1,4 +1,4 @@
 print("Hello user! Welcome to band generator.")
 city = input("What is the name of the city you grew up in?\n")
-pet = input ("What is your pet's name?\n")
+pet = input ("What is your pet name?\n")
 print("Your band name could be " + city + pet)
--- Day_2/tip_calculator.py
+++ Day_2/tip_calculator.py
@@ -1,8 +1,7 @@
-age = input("What is your age? ")
-
-left = 90 - int(age)
-years = int(left) * 365
-weeks = int(left) * 52
-months = int(left) * 12
-
-print(f"remaining days {years}, remaining weeks {weeks}, and remaining months {months}")
+print("Hello user! Welcome to tip calc.")
+total_bill = float(input("What was the total bill in $"))
+tip_percentage = int(input("How much tip you would like to give in percentage? 10%, 15% or your number?"))
+users_amount = int(input("How many people to split the bill?"))
+bill_per_person = (total_bill + total_bill * (tip_percentage / 100)) / users_amount
+final_bill_per_person = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay ${final_bill_per_person}")
